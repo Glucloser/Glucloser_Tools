5a7d99bb40add79e2b208d3958163cfcc7646631
Nathan Lefler <n.lefler@gmail.com>
2016-04-19 22:37:26 -0400

    Carelink download script

---
+++ carelink/carelink_dl.sh
@@ -0,0 +1,37 @@
+
+require 'selenium-webdriver'
+
+def setup
+    profile = Selenium::WebDriver::Firefox::Profile.ini()['Selenium']
+    @driver = Selenium::WebDriver.for :firefox, :profile=>profile
+end
+
+def teardown
+    @driver.quit
+end
+
+def run
+    setup
+      yield
+        teardown
+end
+
+run do
+    @driver.navigate.to 'https://carelink.minimed.com/patient/entry.jsp'
+    @driver.find_element(:id, 'j_username').send_keys ENV['carelink_user']
+    @driver.find_element(:id, 'j_password').send_keys ENV['carelink_pw']
+    @driver.find_element(:id, 'loginButton').click
+    begin
+    Selenium::WebDriver::Wait.new(:timeout => 10).until { @driver.find_element(:id, 'toReports')}
+    rescue Selenium::WebDriver::Error::TimeOutError
+      @driver.save_screenshot('error0.png')
+    end
+    @driver.find_element(:id, 'toReports').click
+    begin
+    Selenium::WebDriver::Wait.new(:timeout => 10).until { @driver.find_element(:link, 'Data Export (CSV)')}
+    rescue Selenium::WebDriver::Error::TimeOutError
+      @driver.save_screenshot('error1.png')
+    end
+    @driver.find_element(:link, 'Data Export (CSV)').click
+    @driver.find_element(:css, '#reportPicker11 > span.reportNav_right > #reportNav_button').click
+end

7034ee61149c6a9181a2544ee7527f75904c7d33
Nathan Lefler <n.lefler@gmail.com>
2016-07-31 15:11:22 -0400

    Use carelink downloaded

--- dl.sh
+++ dl.sh
@@ -3,11 +3,4 @@ SPATH=$(readlink -f "$0")
 DIR=$(dirname "$SPATH")
 
 source "$DIR/.keys.sh"
-
-xvfb-run --server-args="-screen 0 800x600x16" ruby "$DIR/carelink/carelink_dl.rb"
-
-for f in ~/Downloads/CareLink*.csv; do
-  echo "Uploading $f"
-  python "$DIR/post_to_parse.py" $f
-  rm $f
-done
+python $DIR/sync_sensor.py

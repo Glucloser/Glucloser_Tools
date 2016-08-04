1682527ef41710dda86ac08dda5ee504a6a66884
Nathan Lefler <n.lefler@gmail.com>
2016-08-03 21:49:41 -0400

    Send date as date

--- sync_sensor.py
+++ sync_sensor.py
@@ -16,6 +16,32 @@ def hilite(string, status, bold):
         attr.append('1')
     return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
 
+def _create_DATEOBJ(data, tmz_delta, format):
+    date = datetime.strptime(data, format)
+    if tmz_delta:
+        date += tmz_delta
+    return date
+
+def dateToStr(date):
+    # Since datetime doesn't understand parsing timezones
+    return date.strftime("%Y-%m-%dT%H:%M:%S") + "-04:00"
+
+def create_DATE(data, tmz_delta):
+    date = _create_DATEOBJ(data, tmz_delta, "%m/%d/%y")
+    return dateToStr(date)
+
+def create_TIME(data, tmz_delta):
+    date = _create_DATEOBJ(data, tmz_delta, "%H:%M:%S")
+    time_struct = date.timetuple()
+    hour = time_struct[3] * 60 * 60
+    min =  time_struct[4] * 60
+    sec =  time_struct[5]
+    return hour + min + sec
+
+def create_TIMESTAMP(data, tmz_delta):
+    date = _create_DATEOBJ(data, tmz_delta, "%m/%d/%y %H:%M:%S")
+    return dateToStr(date)
+
 session = Carelink.startSession()
 
 CARELINK_USER = os.environ['carelink_user']
@@ -42,10 +68,14 @@ HEADERS = {API_KEY_HEADER: API_KEY, APP_ID_HEADER: APP_ID, "Content-Type": "appl
 
 isTTY = sys.stdout.isatty()
 
+# Don't have any way to get the GMT offset of the device
+# TODO(nl) EST/EDT
+tz_delta = timedelta(hours=4)
+
 for sg in sensor_data.sensorReadings:
     payload = {}
     payload["reading"] = int(sg.reading)
-    payload["dateTimeString"] = sg.dateTimeString
+    payload["dateTimeString"] = dateToStr(create_TIMESTAMP(sg.dateTimeString, tz_delta))
 
     if not payload:
         print "Empty payload, skipping"

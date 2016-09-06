86dc9fad7fec3e04cbb73519462826fabfade3c8
Nathan Lefler <n.lefler@gmail.com>
2016-09-05 21:59:39 -0400

    Send email

--- run_fetch_reports.sh
+++ run_fetch_reports.sh
@@ -3,4 +3,4 @@ SPATH=$(readlink -f "$0")
 DIR=$(dirname "$SPATH")
 
 source "$DIR/.keys.sh"
-python $DIR/download_reports.py
+python $DIR/download_reports.py | xargs -i{} 'echo "" | mutt -s "Daily Report" n.lefler@gmail.com -a {}.pdf'
